b7d004ee20f0f9eb1ff42818cb4e9c7f0f487ced
Duong Pham <phamduong14@gmail.com>
2020-10-06 11:44:07 -0700

    Updates

---
+++ precache-manifest.1ab34070cea20f1b24aad05277cee2a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2107cb82e357c681b7c6eab98c4b1a8b",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "cd3f5eb4128990ad4774",
+    "url": "/portfolio-website/static/css/main.0f89ca09.chunk.css"
+  },
+  {
+    "revision": "a87686d8ba34713fd2d4",
+    "url": "/portfolio-website/static/js/2.ba6f03ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-website/static/js/2.ba6f03ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3f5eb4128990ad4774",
+    "url": "/portfolio-website/static/js/main.38b8d94a.chunk.js"
+  },
+  {
+    "revision": "99cb685b86efd0f580da",
+    "url": "/portfolio-website/static/js/runtime-main.c16362e1.js"
+  }
+]);
